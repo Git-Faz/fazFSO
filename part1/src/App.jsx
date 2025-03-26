00068ba4579e06266cd69a921ffb282ec59bb8b6
Git-Faz <fahad.blr@icloud.com>
2025-03-26 14:52:12 +0530

    part 1 in progress

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course.name}></Header>
+      <Header course={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
     </div>
@@ -27,31 +27,30 @@ const App = () => {
 }
 
 const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
+  return <h1>{props.course}</h1>
 }
 
 const Content = (props) => {
   return (
     <>
-      {props.parts.map((part, index) => (
-        <Part key={index} name={part.name} exercises={part.exercises} />
-      ))}
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
     </>
-  );
+  )
 }
 
 const Part = (props) => {
-  return (<p>{props.name} {props.exercises}</p>)
+  return <p>{props.name} {props.exercises}</p>
 }
 
 const Total = (props) => {
-  const [p1, p2, p3] = [props.parts[0].exercises, props.parts[1].exercises, props.parts[2].exercises];
-  //let sum = p1+p2+p3;
-  return (
-    <p>Total Number of Exercises {p1 + p2 + p3}</p>
-  )
+  const totalExercises = 
+    props.parts[0].exercises + 
+    props.parts[1].exercises + 
+    props.parts[2].exercises;
+
+  return <p>Total Number of Exercises {totalExercises}</p>
 }
 
 export default App;
