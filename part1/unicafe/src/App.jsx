6c8661bf4a69f4c32fffd6242db9ca3f1b9eed6a
Git-Faz <fahad.blr@icloud.com>
2025-04-06 20:48:49 +0530

    unicafe done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,7 +1,14 @@
 import { useState } from "react";
 
-const Button = ({onClick,text}) => <button onClick={onClick} text={text} >{text}</button>
-const Stats = ({text, value = 0}) => <div>{text} {value}</div>
+const Button = ({ onClick, text }) => <button onClick={onClick} text={text} >{text}</button>
+const Stats = ({ text, value = 0 }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
 
 const App = () => {
   const [good, setGood] = useState(0);
@@ -13,20 +20,31 @@ const App = () => {
 
   const addNeutral = () => setNeutral(neutral => neutral + 1);
 
-  const addBad = () => setBad(bad => bad +1);
+  const addBad = () => setBad(bad => bad + 1);
 
-  return(
+  return (
     <>
       <h1>Give Feedback</h1>
       <Button text='Good' onClick={addGood}></Button>
       <Button text='Neutral' onClick={addNeutral}></Button>
       <Button text='Bad' onClick={addBad}></Button>
-
-      <h1>Statistics</h1>
-      <Stats text='Good' value={good}></Stats>
-      <Stats text='Neutral' value={neutral}></Stats>
-      <Stats text='Bad' value={bad}></Stats>
-
+      {(good === 0 && neutral === 0 && bad === 0) ? (
+        <p>No feedback given</p>
+      ) : (
+        <>
+          <h1>Statistics</h1>
+          <table>
+            <tbody>
+              <Stats text='Good' value={good}></Stats>
+              <Stats text='Neutral' value={neutral}></Stats>
+              <Stats text='Bad' value={bad}></Stats>
+              <Stats text={'Total'} value={good + neutral + bad}></Stats>
+              <Stats text={'Average'} value={(good - bad) / (good + neutral + bad)}></Stats>
+              <Stats text={'Positive'} value={good / (good + neutral + bad)}></Stats>
+            </tbody>
+          </table>
+        </>
+      )}
     </>
   )
 }
